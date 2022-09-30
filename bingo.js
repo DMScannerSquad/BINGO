ba253d0191b061898419ca0e9376dba46baf4973
DMScannerSquad <112783470+DMScannerSquad@users.noreply.github.com>
2022-09-29 22:05:43 -0500

    Update bingo.js
    Change free space background color

--- bingo.js
+++ bingo.js
@@ -16,7 +16,7 @@ for (var i = 0; i < squares.length; i++){
 	squares[i].textContent = cardText[i];
 }
 squares[12].textContent = "FREE SPACE";
-squares[12].style.backgroundColor = "#ACA000";
+squares[12].style.backgroundColor = "#fff600";
 	
 function selectSquare(){
 	if (this.style.backgroundColor != "lightskyblue"){
